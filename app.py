942b7534fc214b5c497c8fabdd07debfc7c86150
gene woodstock <genewoodstock@gmail.com>
2022-03-22 20:31:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback_context
 import os
 
 
@@ -8,19 +8,23 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+
+
 app.layout = html.Div([
+    html.Button('Next Question', id='btn-question', n_clicks=0),
     html.H2('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
     html.Div(id='display-value')
 ])
 
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+@app.callback(
+    Output('display-value', 'children'),
+    [Input('btn-question', 'n_clicks')]
+    )
+def display_value(btn_q):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'btn-clicks-1' in changed_id:
+        msg = 'Button 1 was most recently clicked'
+    return f'You have selected {btn_q}'
 
 if __name__ == '__main__':
     app.run_server(debug=True)
